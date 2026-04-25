a8ed157d97a0cf9826491d9d6ab7c3cc66a06d5f
Baraka49-code <barakedward49@gmail.com>
2026-04-25 12:30:06 +0300

    Add files via upload

---
+++ ProfileCard.java
@@ -0,0 +1,40 @@
+public class ProfileCard {
+    public static void main(String[] args){
+
+        //Student name
+        String name ="Baraka Edward Dandu";
+
+        //Student ID
+        String studentId ="2024-04-01318";
+
+        //Year of study
+        String year ="2nd Year";
+
+        //Background
+        String background ="Learned C programming,JSON,Python,HTML,CSS and Javascript.Also driven to learn the Object Oriented Programming in Java";
+
+        //Course goal
+        String goal ="To learn and ,master the Object Oriented Programming in Java and be passionate programmer";
+
+        //Fun fact
+        String funFact ="I enjoy the programming life by solving logic problems gradually from simple to complex ones";
+
+        //Title
+        String title ="CS 234 - JAVA PROFILE CARD";
+
+        //Output alignment
+System.out.println("==================================================================");
+       System.out.println("      " + title);
+System.out.println("==================================================================");
+       System.out.println("Name       : " + name);
+       System.out.println("Student ID : " + studentId);
+       System.out.println("Year       : " + year);
+System.out.println("----------------------------------------------------------------");
+       System.out.println("Background : " + background);
+System.out.println("----------------------------------------------------------------");
+       System.out.println("Goal       : " + goal);
+System.out.println("----------------------------------------------------------------");
+       System.out.println("Fun Fact   : " + funFact);
+System.out.println("==================================================================");
+    }
+}
